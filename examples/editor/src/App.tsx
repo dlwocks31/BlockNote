a32c5a50f6747684adf10cb5d486681bd700d235
Jaechan <dlwocks31@gmail.com>
2023-08-11 22:26:29 +0900

    added types

--- examples/editor/src/App.tsx
+++ examples/editor/src/App.tsx
@@ -1,26 +1,89 @@
-// import logo from './logo.svg'
+import { BlockSchema, defaultBlockSchema, defaultProps } from "@blocknote/core";
 import "@blocknote/core/style.css";
-import { BlockNoteView, useBlockNote } from "@blocknote/react";
-import styles from "./App.module.css";
+import {
+  BlockNoteView,
+  ReactSlashMenuItem,
+  createReactBlockSpec,
+  getDefaultReactSlashMenuItems,
+  useBlockNote,
+} from "@blocknote/react";
+import { RiImage2Fill } from "react-icons/ri";
 
-type WindowWithProseMirror = Window & typeof globalThis & { ProseMirror: any };
-
-function App() {
-  const editor = useBlockNote({
-    onEditorContentChange: (editor) => {
-      console.log(editor.topLevelBlocks);
+export default function App() {
+  // Creates a custom image block.
+  const ImageBlock = createReactBlockSpec({
+    type: "image",
+    propSchema: {
+      ...defaultProps,
+      src: {
+        default: "https://via.placeholder.com/1000",
+      },
+      alt: {
+        default: "image",
+      },
     },
-    editorDOMAttributes: {
-      class: styles.editor,
-      "data-test": "editor",
-    },
-    theme: "light",
+    containsInlineContent: false,
+    render: ({ block }) => (
+      <div
+        onClick={() => {
+          editor.updateBlock(block.id, {
+            props: {
+              src: "https://via.placeholder.com/800",
+            },
+          });
+        }}>
+        <img src={block.props.src} alt={block.props.alt} />
+      </div>
+    ),
   });
 
-  // Give tests a way to get prosemirror instance
-  (window as WindowWithProseMirror).ProseMirror = editor?._tiptapEditor;
+  // The custom schema, which includes the default blocks and the custom image
+  // block.
+  const customSchema = {
+    // Adds all default blocks.
+    ...defaultBlockSchema,
+    // Adds the custom image block.
+    image: ImageBlock,
+  } satisfies BlockSchema;
 
+  // Creates a slash menu item for inserting an image block.
+  const insertImage: ReactSlashMenuItem<typeof customSchema> = {
+    name: "Insert Image",
+    execute: (editor) => {
+      const src: string | null = prompt("Enter image URL");
+      const alt: string | null = prompt("Enter image alt text");
+
+      editor.insertBlocks(
+        [
+          {
+            type: "image",
+            props: {
+              src: src || "https://via.placeholder.com/1000",
+              alt: alt || "image",
+            },
+          },
+        ],
+        editor.getTextCursorPosition().block,
+        "after"
+      );
+    },
+    aliases: ["image", "img", "picture", "media"],
+    group: "Media",
+    icon: <RiImage2Fill />,
+    hint: "Insert an image",
+  };
+
+  // Creates a new editor instance.
+  const editor = useBlockNote({
+    theme: "dark",
+    // Tells BlockNote which blocks to use.
+    blockSchema: customSchema,
+    slashMenuItems: [
+      ...getDefaultReactSlashMenuItems(customSchema),
+      insertImage,
+    ],
+  });
+
+  // Renders the editor instance using a React component.
   return <BlockNoteView editor={editor} />;
 }
-
-export default App;
